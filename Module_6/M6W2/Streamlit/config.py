993de6a5a20815c71beaac6ba41a0eb55105aa4d
Hung <87250641+duchung45644@users.noreply.github.com>
2024-12-17 13:42:19 +0700

    fix weights, labels path

--- Module_6/M6W2/Streamlit/config.py
+++ Module_6/M6W2/Streamlit/config.py
@@ -4,12 +4,13 @@ import torch
 class AppConfig:
     def __init__(self):
         # Paths to weights
-        self.resnet_weights_path = "weights/Sences_DenseNet.pth"
-        self.densenet_weights_path = "weights/Sences_DenseNet.pth"
+        self.resnet_weights_path = "Module_6/M6W2/Streamlit/weights/Sences_DenseNet.pth"
+        self.densenet_weights_path = "Module_6/M6W2/Streamlit/weights/Sences_DenseNet.pth"
+        
 
         # Paths to class label files
-        self.weather_classes_file = "./classes/weather_cls.txt"
-        self.scenes_classes_file = "./classes/scenes_cls.txt"
+        self.weather_classes_file = "Module_6/M6W2/Streamlit/classes/weather_cls.txt"
+        self.scenes_classes_file = "Module_6/M6W2/Streamlit/classes/scenes_cls.txt"
 
         # Device configuration
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
